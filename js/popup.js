21b12b68da7f4df19397f1c8bca096d2cd72a8dc
Robert Sefer <rsefer@gmail.com>
2017-10-01 20:47:00 -0500

    Popup display

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,22 @@
 window.onload = function() {
+
+  var addSiteButton = document.getElementById('not-now-add-this-site-button');
+  addSiteButton.addEventListener('click', function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      var activeTab = tabs[0];
+      var activeTabId = activeTab.id;
+      console.log(activeTab);
+      var currentSiteDomain = extractHostname(activeTab.url);
+      document.getElementsByClassName('add-this-site-label')[0].innerHTML = currentSiteDomain;
+      document.getElementsByClassName('not-now-popup')[0].className += ' adding';
+  });
+
+  });
+
+
   var settingsButton = document.getElementById('not-now-settings-button');
   settingsButton.addEventListener('click', function() {
     chrome.runtime.sendMessage({ type: 'settings' });
   });
+
 };
